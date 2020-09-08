d31949f33ca8b3d586ab5494fee94d6bd2dfba1f
dyuusha <cesarlopezlima1@gmail.com>
2020-09-08 10:00:25 -0600

    firebase, material ui and Router added

--- src/index.js
+++ src/index.js
@@ -3,7 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import firebase from 'firebase';
+const firebaseConfig = {
+  apiKey: "AIzaSyAeVL6H3SCF8ULha6dweU26Z6HSZqsV0NQ",
+  authDomain: "rickchallenge-369f4.firebaseapp.com",
+  databaseURL: "https://rickchallenge-369f4.firebaseio.com",
+  projectId: "rickchallenge-369f4",
+  storageBucket: "rickchallenge-369f4.appspot.com",
+  messagingSenderId: "442917579702",
+  appId: "1:442917579702:web:dd071b5963500b9dd2ef17",
+  measurementId: "G-XD0MBC2MN8"
+};
 
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 ReactDOM.render(
   <React.StrictMode>
     <App />
